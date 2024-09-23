b92414327d3e20a13c5ca6c6f90cfc782f93da91
harunsasmaz <meharunsasmaz@gmail.com>
2024-09-23 21:23:14 +0300

    chore: update templates

--- java-template/src/main/java/com/grpcdemo/instrument/v1/interceptor/StreamServerAuthInterceptor.java
+++ java-template/src/main/java/com/grpcdemo/instrument/v1/interceptor/StreamServerAuthInterceptor.java
@@ -4,9 +4,7 @@ import io.grpc.*;
 
 public class StreamServerAuthInterceptor implements ServerInterceptor {
 
-    private static final Metadata.Key<String> SENDER_NAME_KEY =
-            Metadata.Key.of("x-sender-name", Metadata.ASCII_STRING_MARSHALLER);
-    private static final String AUTHORIZED_SENDER = "midascrypto";
+    
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
@@ -14,6 +12,6 @@ public class StreamServerAuthInterceptor implements ServerInterceptor {
             Metadata headers,
             ServerCallHandler<ReqT, RespT> next) 
     {
-        
+
     }
 }
